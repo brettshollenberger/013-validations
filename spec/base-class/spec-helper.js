c73e7e0fe345c2f166964b95232e549b23b529ca
Brett Shollenberger <brett.shollenberger@gmail.com>
2014-05-03 19:19:06 -0700

    Add registerable validators

--- spec/base-class/spec-helper.js
+++ spec/base-class/spec-helper.js
@@ -1,13 +1,18 @@
-var Mocks, BaseClass, Post, Person, Validatable, Validation;
+var Mocks, BaseClass, Post, Person, Validatable, Validation, ValidationOptions;
 beforeEach(module('BaseClass'));
 beforeEach(module('Mocks'));
 beforeEach(inject(["BaseClass", "Mocks", "BCValidatable", "BCValidatable.Validation", 
-  "BCErrorable", 
-  function(_BaseClass_, _Mocks_, _BCValidatable_, _BCValidation_) {
-    BaseClass   = _BaseClass_;
-    Mocks       = _Mocks_;
-    Post        = Mocks.Post;
-    Person      = Mocks.Person;
-    Validatable = _BCValidatable_;
-    Validation  = _BCValidation_;
+  "BCValidatable.Validator", "BCValidatable.ValidationOptions",
+  function(_BaseClass_, _Mocks_, _BCValidatable_, _BCValidation_, _BCValidator_, 
+    _BCValidationOptions_) {
+
+    BaseClass         = _BaseClass_;
+    Mocks             = _Mocks_;
+    Post              = Mocks.Post;
+    Person            = Mocks.Person;
+    Validatable       = _BCValidatable_;
+    Validation        = _BCValidation_;
+    Validator         = _BCValidator_;
+    ValidationOptions = _BCValidationOptions_;
+
 }]));
--- spec/base-class/validatable/validatable.spec.js
+++ spec/base-class/validatable/validatable.spec.js
@@ -1,29 +1,341 @@
 describe("BCValidatable", function() {
 
-  describe("Constructing individual validations", function() {
-    var nameRequired;
-    beforeEach(function() {
-      nameRequired = new Validation("name", "Name cannot be blank", function(value) {
-        return value !== void 0 && String(value).length > 0;
+  // # Validators
+  // =============
+  //
+  // Validators are registerable, configurable constructors that return validation functions, which
+  // are capable of validating values and reporting error messages.
+  //
+  //    factory('MinValidator', ['Validatable.Validator', function(Validator) {
+  //      function min(value) {
+  //        return value.length >= this.min;
+  //      }
+  //
+  //      min.message = function() {
+  //        return "must be greater than " + this.min + " characters.";
+  //      }
+  //
+  //      return new Validator(min);
+  //    }]);
+  //
+  // By registering this function as a validator, we obtain the ability to use it in our models:
+  //
+  //    Post.validates({
+  //      title: {
+  //        min: 5
+  //      }
+  //    });
+  //
+  // In low-level terms, registered validators expose one function, configure:
+  //
+  //    // example from above
+  //    MinValidator.configure(5);
+  //
+  //    // with additional options
+  //    MinValidator.configure({value: 5, message: "Must be larger than that."});
+  //
+  // ## Define Arbitrary Configuration Options
+  // ========================================== 
+  //
+  // A configured validator returns a validation function, with its error message and options
+  // set. The options are all exposed on the context of the validation function:
+  //
+  //    function numericality(value) {
+  //      if (this.ignore) value = value.replace(this.ignore, '');
+  //      return !(isNaN(value));
+  //    }
+  //
+  //    moneyValidationFn = NumericalityValidator.configure({
+  //      ignore: /^\$/
+  //    });
+  //
+  // Each field on your models can configure the validator with different options.
+  //
+  // ## Registering Validators with Children
+  // ========================================
+  //
+  // Sometimes we want to describe a collection of validations under the same name in our DSL:
+  //
+  //    Post.validates({
+  //      title: {
+  //        length: {
+  //          min: 5,
+  //          max: 10
+  //        }
+  //      }
+  //    });
+  //
+  // Here we say that the length validator has two children--in this example, the user is actually
+  // naming two separate validators--min & max length.
+  //
+  // To create this grouping of validators:
+  //
+  // 1) Create the children
+  //
+  //    factory('MinValidator', ['Validator', function() {
+  //      ...
+  //      return new Validator(min);
+  //    }]);
+  //
+  //    factory('MaxValidator', ['Validator', function() {
+  //      ...
+  //      return new Validator(max);
+  //    }]);
+  //
+  // 2) Name the children as options of the parent:
+  //
+  //    factory('LengthValidator', ['MinValidator', 'MaxValidator', 'Validatable.Validator', 
+  //      function(min, max, Validator) {
+  //
+  //      function length() {}
+  //
+  //      length.options = {
+  //        min: minValidator,
+  //        max: maxValidator
+  //      }
+  //
+  //      return new Validator(length);
+  //    }]);
+  //
+  // Configuring LengthValidator will now configure any associated children:
+  //
+  //    // example above
+  //    LengthValidator.configure({
+  //      min: 5,
+  //      max: 10
+  //    });
+  //
+  //    // calls
+  //    MinValidator.configure(5);
+  //    MaxValidator.configure(10);
+  //
+  //    // returns
+  //    [configuredMinFn, configuredMaxFn]
+  //
+  // ## Interaction with the Validatable Module
+  // ===========================================
+  //
+  // Validatable itself exposes the method #validates, which sets the collection of validations for 
+  // each model it is mixed into. When it receives a configuration like:
+  //
+  //    Post.validates({
+  //      title: {
+  //        required: true,
+  //        length: { in: _.range(1, 20) }
+  //      },
+  //      slug: {
+  //        ...
+  //      }
+  //    });
+  //
+  // It loops through each field, takes the name of each validation, and configures the validation,
+  // passing in the options provided. It will either receive in return a single configured validation
+  // function, or an array of configured validation functions (as in the case of the LengthValidator).
+  //
+  // It transforms each validation function it receives into a Validation--a combination of the
+  // field name, validation function, and message.
+  //
+  // When a Validatable model calls #validate([field]) on a particular instance, under the covers 
+  // Validatable checks the validations for the specified field (or all fields if none is provided),
+  // and hands off the request to each Validation. Validation#validate receives an instance, and
+  // already knows which field to validate, which configured validation function to call, and
+  // what error message to return in the case the value is invalid.
+  //
+  describe("Validators", function() { 
+    describe("Basic validators", function() {
+      var requiredFn, requiredValidator, configuredRequiredFn;
+      beforeEach(function() {
+        requiredFn = function required(value) {
+          return value !== void 0 && !_.isUndefined(value.length) && value.length > 0;
+        }
+
+        requiredFn.message = "cannot be blank.";
+
+        requiredValidator    = new Validator(requiredFn);
+        configuredRequiredFn = requiredValidator.configure(true);
+      });
+
+      it("configures and returns a new validationFn based on the configuration", function() {
+        expect(configuredRequiredFn("")).toBe(false);
+        expect(configuredRequiredFn("hello")).toBe(true);
+      });
+
+      it("configures the message for the validation", function() {
+        expect(configuredRequiredFn.message).toBe("cannot be blank.");
+      });
+
+      it("sets default options", function() { 
+        expect(requiredValidator.options).toEqual(["value", "message", "required"]);
+      });
+
+      it("sets the message", function() { 
+        expect(requiredValidator.message).toEqual("cannot be blank.");
       });
     });
 
-    it("validates a particular field", function() {
-      expect(nameRequired.field).toEqual("name");
+    describe("Validators that receive values", function() {
+      var minValidator, configuredMinFn;
+      beforeEach(function() { 
+        function min(value) {
+          return value.length >= this.min;
+        }
+
+        min.message = function() {
+          return "Must be greater than " + this.min;
+        }
+
+        minValidator    = new Validator(min);
+        configuredMinFn = minValidator.configure(5);
+      });
+
+      it("curries in the configured values to the validationFn", function() { 
+        expect(configuredMinFn("hello")).toEqual(true);
+        expect(configuredMinFn("hi")).toEqual(false);
+      });
     });
 
-    it("contains a validation message", function() {
-      expect(nameRequired.message).toEqual("Name cannot be blank");
+    describe("Validators with children", function() {
+      var lengthValidator, minValidator, maxValidator, configuredLengthFunctions, configuredMinFn,
+          configuredMaxFn;
+
+      beforeEach(function() { 
+        function min(value) {
+          return value.length >= this.min;
+        }
+
+        min.message = function() {
+          return "Must be more than " + this.min;
+        }
+
+        minValidator = new Validator(min);
+
+        function max(value) {
+          return value.length <= this.max;
+        }
+
+        max.message = function() {
+          return "Must be less than " + this.max;
+        }
+
+        maxValidator = new Validator(max);
+
+        function length() {}
+
+        length.message = "is not the correct length.";
+
+        length.options = {
+          min: minValidator,
+          max: maxValidator
+        }
+
+        lengthValidator           = new Validator(length);
+
+        configuredLengthFunctions = lengthValidator.configure({min: 5, max: 10});
+        configuredMinFn           = configuredLengthFunctions[0];
+        configuredMaxFn           = configuredLengthFunctions[1];
+      });
+
+      it("configures each child validator", function() { 
+        expect(configuredMinFn("hi")).toBe(false);
+        expect(configuredMaxFn("hello")).toBe(true);
+      });
+
+      it("configures the default message for each child validation if no configuration is set", function() {
+        expect(configuredMinFn.message).toBe("Must be more than 5");
+        expect(configuredMaxFn.message).toBe("Must be less than 10");
+      });
+
+      it("uses configuration options to override the default message", function() {
+        var lengthConfiguration = {
+          min: {value: 5, message: "Must be longer than that."},
+          max: {value: 10, message: function() { return "I just like the number " + this.max; } }
+        };
+
+        var configuredLengthFunctions = lengthValidator.configure(lengthConfiguration),
+            configuredMinFn           = configuredLengthFunctions[0],
+            configuredMaxFn           = configuredLengthFunctions[1];
+
+        expect(configuredMinFn.message).toBe("Must be longer than that.");
+        expect(configuredMaxFn.message).toBe("I just like the number 10");
+      });
     });
 
-    it("validates values affirmatively", function() {
-      expect(nameRequired.validate("hello")).toBe(true);
-    });
+    describe("Validators with special options", function() {
+      var numericalityValidator, configuredNumericalityFn;
+      beforeEach(function() {
+        function numericality(value) {
+          if (this.ignore) value = value.replace(this.ignore, '');
+          return !(isNaN(value))
+        }
 
-    it("validates values negatively", function() {
-      expect(nameRequired.validate("")).toBe(false);
+        numericality.message = function() {
+          return "Must be a number";
+        }
+
+        var validationConfiguration = {
+          ignore: /^\$/,
+          message: function() {
+            return "Must be a number. Can include " + String(this.ignore);
+          }
+        }
+
+        numericalityValidator    = new Validator(numericality);
+        configuredNumericalityFn = numericalityValidator.configure(validationConfiguration);
+      });
+
+      it("passes custom configuration options into the validationFn", function() { 
+        expect(configuredNumericalityFn("$5.00")).toBe(true);
+      });
+
+      it("passes custom configuration options into the message", function() { 
+        expect(configuredNumericalityFn.message).toBe("Must be a number. Can include /^\\\$/");
+      });
     });
   });
+
+  // # Validations
+  // ==============
+  //
+  // Validations receive a field name to validate, and a configured validation function.
+  //
+  // They expose the public method #validate, which receives an object to validate. They use
+  // the field name to ensure that the value on that particular field is valid, and return true
+  // or false.
+  //
+  // They also expose the error message of the validation function.
+  ddescribe("Validations", function() {
+    describe("Constructing individual validations", function() {
+      var minValidator, configuredMinFn, MinValidation;
+      beforeEach(function() {
+        function min(value) {
+          return value.length >= this.min;
+        }
+
+        min.message = function() {
+          return "Must be greater than " + this.min;
+        }
+
+        minValidator    = new Validator(min);
+        configuredMinFn = minValidator.configure(5);
+        MinValidation   = new Validation("name", configuredMinFn);
+      });
+
+      it("validates a particular field", function() {
+        expect(MinValidation.field).toEqual("name");
+      });
+
+      it("contains the validation message of the configured validationFn", function() {
+        expect(MinValidation.message).toEqual("Must be greater than 5");
+      });
+
+      it("validates the specified field using the validation function", function() {
+        expect(MinValidation.validate({name: "a"})).toBe(false);
+        expect(MinValidation.validate({name: "Jon Jacob Jinglehemier Schmidt"})).toBe(true);
+        expect(MinValidation.validate({age: 1, name: "At least 5"})).toBe(true);
+      });
+    });
+  });
+
 
   describe("Constructing a set of validations for a model", function() {
     var person;
@@ -38,7 +350,7 @@ describe("BCValidatable", function() {
         },
         age: {
           required: {message: "is required"},
-          legal: {validator: function(age) { return Number(age) >= 21; },
+          legal: {validator: function(age) { return function(age) { return Number(age) >= 21; }},
                   message: "must be at least 21"}
         }
       });
@@ -57,6 +369,12 @@ describe("BCValidatable", function() {
       expect(Person.validations.name[1].field).toEqual("name");
     });
 
+    it("passes options to validation messages if necessary", function() {
+      person.name = "A";
+      person.validate();
+      expect(person.$errors.name).toContain("Must be at least 5 characters");
+    });
+
     it("adds custom error messages", function() {
       expect(Person.validations.age[0].message).toEqual("is required");
     });
@@ -72,6 +390,7 @@ describe("BCValidatable", function() {
       person.age = 20;
       expect(person.validate()).toBe(false);
       person.age = 21;
+      console.log(person.$errors);
       expect(person.validate()).toBe(true);
     });
 
